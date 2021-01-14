01580c5082ec32488bcfbc5538be772e95263f4a
git <abonini2000@gmail.com>
2021-01-14 00:08:23 -0300

    routing :metal: :hurtrealbad:

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -3,14 +3,21 @@ import { Link, routes } from '@redwoodjs/router'
 const AboutPage = () => {
   return (
     <>
-      <h1>AboutPage</h1>
-      <p>
+       <h1><Link to={routes.home()}>Horse Blog</Link></h1>
+       <h1>AboutPage</h1>
+       <nav>
+      <ul>
+        <li><Link to={routes.about()}>AboutPage</Link></li>
+      </ul>
+
+</nav>
+      {/* <p>
         Find me in <code>./web/src/pages/AboutPage/AboutPage.js</code>
       </p>
       <p>
         My default route is named <code>about</code>, link to me with `
         <Link to={routes.about()}>About</Link>`
-      </p>
+      </p> */}
     </>
   )
 }
